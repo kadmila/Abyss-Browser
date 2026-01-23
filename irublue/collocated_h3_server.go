d144fa4ce5443cfd50117c331bfb8c34dca18506
MinwooWebeng <kadmila.abyss@gmail.com>
2026-01-24 01:42:00 +0900

    .

--- irublue/collocated_h3_server.go
+++ irublue/collocated_h3_server.go
@@ -7,14 +7,20 @@ import (
 	"github.com/quic-go/quic-go/http3"
 )
 
-func NewCollocatedH3Server(cert tls.Certificate, handler http.Handler) *http3.Server {
+func NewCollocatedH3Server(cert tls.Certificate) *http3.Server {
 	return &http3.Server{
 		Addr:    ":4433",
-		Handler: handler,
+		Handler: NewAuthService(),
 		TLSConfig: &tls.Config{
 			Certificates:          []tls.Certificate{cert},
 			ClientAuth:            tls.RequireAnyClientCert,
 			VerifyPeerCertificate: VerifyCollocatedHttp3PeerCertificate,
 		},
 	}
+}
+
+func NewAuthService() http.Handler {
+	mux := http.NewServeMux()
+
+	return mux
 }
