8d32a4d6c6048a5a530b77f7e60f2169e8414ca8
MinwooWebeng <kadmila.abyss@gmail.com>
2026-01-27 14:51:39 +0900

    .net included

--- abyss_engine/build_release.ps1
+++ abyss_engine/build_release.ps1
@@ -6,4 +6,5 @@ cd ..
 
 python.exe ./Tool/ExternData.py
 
-dotnet build AbyssCLI.csproj --configuration Release
+#dotnet build AbyssCLI.csproj --configuration Release
+dotnet publish -c Release -r win-x64 --self-contained true
